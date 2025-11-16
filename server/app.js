e33282cc03a303126682542b327d2fe543c7c4ed
Jay442 <jaygw1188@gmail.com>
2025-11-16 18:09:13 -0500

    part 1 fixed

--- server/app.js
+++ server/app.js
@@ -12,28 +12,28 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
-    if(err) {
+  if (err) {
     console.log('Could not connect to database');
     throw err;
-    }
+  }
 });
 
 const app = express();
 
 app.use(helmet());
-app.use('assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: ''}));
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 
 router(app);
 
 app.listen(port, (err) => {
-    if(err) {throw err;}
-    console.log(`Listening on port ${port}`);
+  if(err) { throw err; }
+  console.log(`Listening on port ${port}`);
 });
